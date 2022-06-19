836080b380f38d40c2d8664c379c6c7fd1ba9579
Leonardo Lautenschlaeger <70241645+leolautens@users.noreply.github.com>
2022-06-19 20:20:23 -0300

    create method DELETE parking spot (UUID id)

--- parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -59,4 +59,14 @@ public class ParkingSpotController {
                 -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found."));
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id") UUID id){
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (parkingSpotModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+        }
+        parkingSpotService.delete(parkingSpotModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully.");
+    }
+
 }
--- parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -43,4 +43,9 @@ public class ParkingSpotService {
     public Optional<ParkingSpotModel> findById(UUID id) {
         return parkingSpotRepository.findById(id);
     }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
+    }
 }
